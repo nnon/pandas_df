08be548ab3b257f8f4783b02ee0a44ff9f209a11
nnon <remackinnon@gmail.com>
2019-12-27 21:57:57 +0000

    Added examples

--- df.py
+++ df.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 # intialise data of lists.
-data = {'Name':['Tom', 'nick', 'krish', 'jack'],
-        'Age':[20, 21, 19, 18]}
+data = {'Name':['Tom', 'Nick', 'Krish', 'Jack', 'Sarah', 'Jane'],
+        'Age':[20, 21, 19, 18, 20, 21],
+        'Gender':['M','M','M','M','F','F']}
 
 # Create DataFrame
 df = pd.DataFrame(data)
@@ -10,22 +11,32 @@ df = pd.DataFrame(data)
 # Print the output.
 print(df)
 
+# Basic query
+print(df.query('Age > 20'))
+print(df.query('Name == "Tom"'))
 
 # Add a column
-df.insert(2, 'Shoe size', [10,11,12,11], False)
+df.insert(2, 'Shoe size', [10,11,12,11,5,4], False)
 print(df)
 
-height = [160, 170, 180, 190]
-df['height'] = height
+height = [160, 170, 180, 190, 120, 130]
+df['Height'] = height
 print(df)
 
-df2 = df.assign(weight = [200, 250, 300, 350])
-print(df2)
+df1a = df.assign(weight = [200, 250, 300, 350, 120, 140])
+print(df1a)
 
 
 # Add a row
 
 # Create a 2nd DataFrame
+
+df2 = pd.DataFrame({'Name':['Tom2', 'Nick2', 'Krish2', 'Jack2'],
+                    'Age':[20, 21, 19, 18],
+                    'Gender':['M','M','M','M'],
+                    'Shoe size':[10,11,12,11],
+                    'Height':[120,130,140,150]})
+print(df2)
 
 # Left join
 
@@ -34,7 +45,14 @@ print(df2)
 # Outer join
 
 # Union
+df3 = df.append(df2)
+print(df3)
 
 # Minus
 
 # Index
+
+# Group by
+print(df3.groupby(['Gender']).mean())
+print(df3.groupby(['Gender']).min())
+print(df3.groupby(['Gender']).max())
